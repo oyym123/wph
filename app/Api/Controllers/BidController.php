56432fcd0ad6c4a7f93bae7febd98efe2c632e87
ouyym <13161057904@163.com>
2018-08-14 11:08:03 +0800

    修改

--- app/Api/Controllers/BidController.php
+++ app/Api/Controllers/BidController.php
@@ -17,69 +17,7 @@ class BidController extends WebController
     /**
      * @SWG\Post(path="/api/bid/bidding",
      *   tags={"竞拍"},
-     *   summary="每个用户竞拍时的接口",
-     *   description="Author: OYYM",
-     *   @SWG\Parameter(name="token", in="header", default="1", description="用户token" ,required=true,
-     *     type="string",
-     *   ),
-     *   @SWG\Parameter(name="period_id", in="formData", default="16", description="", required=true,
-     *     type="string",
-     *   ),
-     *   @SWG\Response(
-     *       response=200,description="
-     *              [status] => 10 = 请求成功,但是竞拍失败，当获取到这个状态时，刷新倒计时为10秒
-     *              "
-     *   )
-     * )
-     */
-    public function bidding()
-    {
-        $this->auth();
-        $bid = new Bid();
-        $bid->userIdent = $this->userIdent;
-        $bid->userId = $this->userId;
-        self::showMsg($bid->personBid($this->request->period_id));
-    }
-
-
-    /**
-     * @SWG\Get(path="/api/bid/record",
-     *   tags={"竞拍"},
-     *   summary="出价记录",
-     *   description="Author: OYYM",
-     *   @SWG\Parameter(name="period_id", in="query", default="16", description="",
-     *     type="string",
-     *   ),
-     *   @SWG\Parameter(name="limit", in="query", default="20", description="个数",
-     *     type="string",
-     *   ),
-     *   @SWG\Response(
-     *       response=200,description="
-     *              [bid_price] => 价格
-     *              [bid_time] => 2018-07-26 17:56:40
-     *              [nickname] => 昵称
-     *              [avatar] => 头像
-     *              [area] => 地址
-     *              [bid_type] => 0 = 出局 , 1 = 领先
-     *     "
-     *   )
-     * )
-     */
-    public function record()
-    {
-        $bid = new Bid();
-        if ($this->request->limit) {
-            $bid->limit = $this->request->limit;
-        } else {
-            $bid->limit = 100;
-        }
-        self::showMsg($bid->bidRecord($this->request->period_id));
-    }
-
-    /**
-     * @SWG\Post(path="/api/bid/auto",
-     *   tags={"竞拍"},
-     *   summary="自动竞拍提交数据",
+     *   summary="竞拍提交数据",
      *   description="Author: OYYM",
      *   @SWG\Parameter(name="token", in="header", default="1", description="用户token" ,required=true,
      *     type="string",
@@ -95,13 +33,18 @@ class BidController extends WebController
      *   )
      * )
      */
-    public function auto()
+    public function bidding()
     {
         $this->auth();
-        $autoBid = new AutoBid();
-        $autoBid->userIdent = $this->userIdent;
-        $autoBid->userId = $this->userId;
-        self::showMsg($autoBid->submitInfo($this->request));
+        if ($this->request->times == 1 || empty($this->request->times)) {
+            $bid = new Bid();
+            self::showMsg($bid->personBid($this->request->period_id));
+        } else {
+            $autoBid = new AutoBid();
+            $autoBid->userIdent = $this->userIdent;
+            $autoBid->userId = $this->userId;
+            self::showMsg($autoBid->submitInfo($this->request));
+        }
     }
 
 
