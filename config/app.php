f97742420a3aadcdd9ed5c5cd57571f471a02a74
ouyym <13161057904@163.com>
2018-09-03 10:28:42 +0800

    修改

--- config/app.php
+++ config/app.php
@@ -179,7 +179,7 @@ return [
         Encore\Admin\AdminServiceProvider::class,
         Overtrue\LaravelWechat\ServiceProvider::class,
         zgldh\QiniuStorage\QiniuFilesystemServiceProvider::class,
-        Maatwebsite\Excel\ExcelServiceProvider::class,
+       // Maatwebsite\Excel\ExcelServiceProvider::class,
 
     ],
 
@@ -209,7 +209,7 @@ return [
         'DB' => Illuminate\Support\Facades\DB::class,
         'Eloquent' => Illuminate\Database\Eloquent\Model::class,
         'Event' => Illuminate\Support\Facades\Event::class,
-        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
+       // 'Excel' => Maatwebsite\Excel\Facades\Excel::class,
         'File' => Illuminate\Support\Facades\File::class,
         'Gate' => Illuminate\Support\Facades\Gate::class,
         'Hash' => Illuminate\Support\Facades\Hash::class,
